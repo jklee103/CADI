d1bb95d498eeeaf71fecd30245969f689d19b00f
이재경 <jklee103@gmail.com>
2017-02-11 00:48:59 +0900

    1week

---
+++ MyApplication2/app/src/main/java/com/example/a0/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.a0.myapplication;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.support.v7.*;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
